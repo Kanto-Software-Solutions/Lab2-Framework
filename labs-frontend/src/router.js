6524b723a9f96926f2cf802f1126723562dd9f98
nirojasm-u <nirojasm@unal.edu.co>
2023-10-04 22:47:52 -0500

    Router Children

--- labs-frontend/src/router.js
+++ labs-frontend/src/router.js
@@ -43,7 +43,12 @@ export default new Router({
           path: "roles",
           name: "roles",
           component: Roles
-        }
+        },
+        {
+          path: "crear-curso",
+          name: "add-course",
+          component: Course
+        },
       ]
     }
   ]
